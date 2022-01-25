624f307625a139136968887248f618f1c851def6
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-01-25 17:58:10 +0530

    added: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import "./styles/index.css"
 import { memo, useState } from "react";
 import Paragraph from "./components/Paragraph"
 import WORD_CLOUD from './utils/word_cloud'
+import Header from "./components/Header";
 
 const App = memo(()=>{
     const [cloud_index, setClouIndex] = useState(Math.floor((Math.random()*WORD_CLOUD.length)))
     return(
         <>
+            <Header heading='React Typing Speed Game'/>
             <Paragraph text={WORD_CLOUD[cloud_index].split(" ")}/>
         </>
     )
