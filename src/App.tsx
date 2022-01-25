321cee06ad116d82097868ae2779aedf33f0e2e4
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-01-25 18:02:23 +0530

    added css added input component for typing git commit -am

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,24 @@ import { memo, useState } from "react";
 import Paragraph from "./components/Paragraph"
 import WORD_CLOUD from './utils/word_cloud'
 import Header from "./components/Header";
+import Input from "./components/Input";
 
 const App = memo(()=>{
     const [cloud_index, setClouIndex] = useState(Math.floor((Math.random()*WORD_CLOUD.length)))
+
+    const [state,setState] = useState('')
+
+    const handleChange = (value:string) =>{
+        setState(value)
+    }
+
+
+
     return(
         <>
             <Header heading='React Typing Speed Game'/>
             <Paragraph text={WORD_CLOUD[cloud_index].split(" ")}/>
+            <Input state={state} handleChange={handleChange}/>
         </>
     )
 })
