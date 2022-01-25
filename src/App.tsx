81e906c8f74dc1f94ce27922abb33b2f77b56fa9
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-01-25 20:03:30 +0530

    added button for changing the paragraph or rendering the new content in it added css for input box and displaying the timer

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,26 @@ import WORD_CLOUD from './utils/word_cloud'
 import Header from "./components/Header";
 import Input from "./components/Input";
 
+const randomNumber =  () => Math.floor((Math.random()*WORD_CLOUD.length))
 const App = memo(()=>{
-    const [cloud_index, setClouIndex] = useState(WORD_CLOUD[Math.floor((Math.random()*WORD_CLOUD.length))].split(" "))
+    const [cloud_index, setCloudIndex] = useState(WORD_CLOUD[randomNumber()].split(" "))
 
     const [state,setState] = useState('')
     const [active_word_index, setActiveWordIndex] = useState(0)
     const [typed_words_arr, setTypedWordsArr] = useState<boolean[]>([])
     const [total_right_words,setTotalRightWords] = useState(0)
     const [total_time, setTotalTime] = useState(0)
+    const [timer, setTimer] = useState<NodeJS.Timeout>()
 
-    let timer:NodeJS.Timer
-    const startTimer = () => timer = setInterval(()=>setTotalTime(v=>v+1),1000)
+    const startTimer = () => setTimer(setInterval(()=>setTotalTime(v=>v+1),1000))
 
     useEffect(()=>{
         if(active_word_index > cloud_index.length-1){
-            clearInterval(timer)
+            setTotalTime(0)
             setTimeout(()=>alert('your typing speed is '+Math.round((total_right_words*60)/total_time)+' wpm'),1000)
+            if(timer){
+                clearInterval(timer)
+            }
             setTypedWordsArr([])
             setActiveWordIndex(0)
             setTotalRightWords(0)
@@ -33,8 +37,8 @@ const App = memo(()=>{
     },[total_time])
 
     const handleChange = (value:string) =>{
+        if(total_time === 0) setTotalTime(1);
         if(value.endsWith(' ')){
-            if(total_time === 0) setTotalTime(1);
             (value.trim() === cloud_index[active_word_index]) && setTotalRightWords(index=>index+1)
             setActiveWordIndex(index=>index+1)
             setState('')
@@ -53,12 +57,20 @@ const App = memo(()=>{
     return(
         <>
             <Header heading='React Typing Speed Game'/>
-            <Paragraph
-                text={cloud_index}
-                active_word_index={active_word_index}
-                typed_words_arr={typed_words_arr}
-            />
-            <Input state={state} handleChange={handleChange}/>
+            <main>
+                <div>
+                    <h2>Type correctly the words, which are present below</h2>
+                    <span>Timer : {total_time}</span>
+                </div>
+                <Paragraph
+                    text={cloud_index}
+                    active_word_index={active_word_index}
+                    typed_words_arr={typed_words_arr}
+                />
+                <Input state={state} handleChange={handleChange} placeholder="type here to begin the game"/>
+                <button onClick={()=>setCloudIndex(WORD_CLOUD[randomNumber()].split(" "))}>change the content</button>
+            </main>
+
         </>
     )
 })
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,12 @@
 import { memo } from "react"
 
-const Input = memo(({state,handleChange}:{
+const Input = memo(({state,handleChange,placeholder}:{
     state:string,
-    handleChange:(value:string)=>void
+    handleChange:(value:string)=>void,
+    placeholder?:string
 })=>{
     return(
-        <input type="text" value={state} onChange={(e)=>handleChange(e.target.value)}/>
+        <input type="text" value={state} onChange={(e)=>handleChange(e.target.value)} placeholder={placeholder}/>
     )
 })
 export default Input
